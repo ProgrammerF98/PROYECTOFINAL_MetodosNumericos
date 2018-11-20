dbaf13c6162bd256f831e38f65685cacc347bf71
ProgrammerF98 <pacheco.solis@outlook.com>
2018-11-19 22:55:27 -0600

    Create Codigo.py

---
+++ Parcial1/PuntoFijo/Codigo.py
@@ -0,0 +1,14 @@
+def xnew(x):
+    return (2*x**2 + 3)/5
+x0 = 0
+x1 = 0
+itera = 0
+for i in range(100):
+    x1 = xnew(x0)
+    if abs(x1-x0) < 0.001:
+        break
+    x0 = x1
+    itera += 1
+
+print("La raíz es %.5f"%x1)
+print("Usando %d iteraciones"%itera)
